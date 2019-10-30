3a6898df95a2ac23bd9a298799a3c0c2e7734ea5
sarthaka1310 <56290744+sarthaka1310@users.noreply.github.com>
2019-10-30 15:10:58 +0530

    Updated factors.c
    Better time complexity.

--- factors.c
+++ factors.c
@@ -1,17 +1,29 @@
 //C program to find factors of any inputted number.
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
 int main()
 {
   int num,i;
   printf("Enter a number:");
   scanf("%d",&num);
   printf("Factors of a number=%d\n",num);
-  for(i=1;i<=num;i++)
+  n=sqrt(num);
+  for(i=1;i<=n;i++)
   {
     if(num%i==0)
     {
-      printf("%d\t",i);
-    }
+      if((i*i)!=num))
+      {
+        printf("%d\t",i,(n/i));
+      }
+      else
+      {
+        printf("%d\t",i);
+      }
+
+   }
   }
 }
