476f6ede2c0effb65985790f1cd53aa899c913b6
hariomsingh311 <70486485+hariomsingh311@users.noreply.github.com>
2020-10-01 01:12:32 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,18 +1,18 @@
 //C program to display Fibonacci sequence.
 
-#include<stdio>
+#include<stdio.h>
 int main()
 {
-  int n1=0,n2=1,t,a,i;
   printf("Enter no of terms:");
   scanf("%d",&a);
+  int arr[a];
+  arr[0]=0;
+  arr[1]=1;
 
   printf("Fibonacci sequence:");
-  for(i=1;i<=a;i++)
+  for(i=2;i<=a+1;i++)
   {
-    printf("%d\t",n1);
-    t=n1+n2;
-    n1=n2;
-    n2=t;
+    arr[i]=arr[i-1]+arr[i-2];
+    printf("%d\t",arr[i-2]);
 }
 }
