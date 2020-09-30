e3032b45953ac092fa7e28b09ee73f2207540062
hariomsingh311 <70486485+hariomsingh311@users.noreply.github.com>
2020-10-01 00:20:27 +0530

    Update additionarray.c

--- additionarray.c
+++ additionarray.c
@@ -1,25 +1,25 @@
 #include<stdio.h>
 int main()
 {
-  int arr1[5],arr2[5],arr3[5],i;
-  printf("Enter 5 numbers of arr1:");
-  for(i=0;i<=4;i++)
-  {
-    scanf("%d",&arr1[i]);
-  }
-  printf("Enter 5 numbers of arr2:");
-  for(i=0;i<=4;i++)
-  {
-    scanf("%d",&arr2[i]);
-  }
-  for(i=0;i<=4;i++)
-  {
-    arr3[i]=arr1[i]+arr2[i];
-  }
-  printf("Addition of two array:");
-  for(i=0;i<=4;i++)
-  {
+  int arr1[n],arr2[n],arr3[n],i,n;
+  printf("Enter Size: ");    //Taking array size 
+  scanf("%d",&n);
 
-      printf("%d\t",arr3[i]);
+
+  printf("Enter %d numbers of arr1:",n);
+  for(i=0;i<n;i++)    
+    scanf("%d",&arr1[i]);    //input in array 1
+
+  printf("Enter %d numbers of arr2:",n);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&arr2[i]);      // input in array 2
+    arr3[i]=arr1[i]+arr2[i];   //Adding elements of array1 and array2
   }
+  
+  printf("Addition of two arrays:  ");
+  for(i=0;i<n;i++)
+      printf("%d\t",arr3[i]);   //printing result
+  
 }
+
