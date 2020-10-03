314a6446bcc9ca89a2a8b15c5d868c3a1a09955a
Git2sep <66361624+Git2sep@users.noreply.github.com>
2020-10-03 20:35:44 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()  //main function
 {
-  double a,b;
-  printf("Enter two numbers:");
-  scanf("%ld%ld",&a,&b);
-  printf("Addition of two numbers=%ld",a+b);
+  int a,b;
+  printf("Enter two numbers:\n");
+  scanf("%d%d",&a,&b);
+  
+  printf("Addition of two numbers = %d",a+b);
   return 0;
 }
