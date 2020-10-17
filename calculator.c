070a4cf2992b9e93a57a05f14b9dfd275abe497c
Mayank Aggarwal <70526600+Mayankagg44@users.noreply.github.com>
2020-10-17 16:37:34 +0530

    More options in the Calculator
    More options in the Calculator

--- calculator.c
+++ calculator.c
@@ -1,31 +1,49 @@
 //C program to display simple calculator.
 
-#include<stdio.h>
+#include <stdio.h>
+#include<math.h> 
 int main()
 {
-  float n1,n2;
-  char operator;
-  printf("Enter an operator(+,-,/,*,^):");
-  scanf("%c",&operator);
+    int num1,num2;
+    float result;
+    char ch;         
+    printf("Enter first number: ");
+    scanf("%d",&num1);
+    printf("Enter second number: ");
+    scanf("%d",&num2);
+    printf("Choose operation to perform (+,-,*,/,%,^): ");
+    scanf(" %c",&ch);
+     
+    result=0;
+    switch(ch)    
+    {
+        case '+':
+            result=num1+num2;
+            break;
+             
+        case '-':
+            result=num1-num2;
+            break;
+         
+        case '*':
+            result=num1*num2;
+            break;
+             
+        case '/':
+            result=num1 / num2;
+            break;
+             
+        case '%':
+            result=num1 % num2;
+            break;
 
-  printf("Enter two numbers:");
-  scanf("%f%f",&n1,&n2);
-  
-  switch(operator) 
-  {
-      
-    case'+' : printf("%f + %f = %f",n1,n2,n1+n2);
-              break;
-    case'-' : printf("%f - %f = %f",n1,n2,n1-n2);
-              break;
-    case'/' : printf("%f / %f = %f",n1,n2,n1/n2);
-              break;
-    case'*' : printf("%f * %f = %f",n1,n2,n1*n2);
-              break;
-    case'^' : printf("%f ^ %f = %f",n1,n2,n1*n2);
-              break;
-    default: printf("Error!operator is wrong");
+        case '^':
+            result=pow(num1,num2);
+            break;
 
-  }
-  return 0;
+        default:
+            printf("Invalid operation.\n");
+    }
+    printf("Result: %d %c %d = %0.2f\n",num1,ch,num2,result);
+    return 0;
 }
