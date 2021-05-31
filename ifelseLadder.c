044a67bbcd138185f7f45eddf70aaad560a5dc8e
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-05-31 12:10:01 +0530

    Create ifelseLadder.c
    This file has an example of an if-else Ladder statement. 
    Program to relate two integers using =, > or < symbol

---
+++ ifelseLadder.c
@@ -0,0 +1,25 @@
+// Program to relate two integers using =, > or < symbol
+
+#include <stdio.h>
+int main() {
+    int number1, number2;
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    //checks if the two integers are equal.
+    if(number1 == number2) {
+        printf("Result: %d = %d",number1,number2);
+    }
+
+    //checks if number1 is greater than number2.
+    else if (number1 > number2) {
+        printf("Result: %d > %d", number1, number2);
+    }
+
+    //checks if both test expressions are false
+    else {
+        printf("Result: %d < %d",number1, number2);
+    }
+
+    return 0;
+}
