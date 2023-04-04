75ccad375fdddc48551b4d8ca373ca85ba91c469
Transyltooniaa <Ajitesh.Kumar@iiitb.ac.in>
2023-04-05 01:08:28 +0530

    reduced iterations

--- factorial.c
+++ factorial.c
@@ -6,7 +6,7 @@ int main()
   int i,a,fact=1;
   printf("Enter a number:");
   scanf("%d",&a);
-  for(i=1;i<=a;i++)
+  for(i=2;i<=a;i++) // we start with 2 because 1 multiplied by number returns number itself . we reduce iteration by 1
   {
     fact=fact*i;
   }
