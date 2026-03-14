36469b9498016f5e67e1fd56ae861a6f21792287
corn <cornjar9@gmail.com>
2026-03-14 17:21:30 +0100

    Fixed calculator so it functions properly.

--- calculator.c
+++ calculator.c
@@ -1,36 +1,21 @@
-#include<stdio.h>
-int main()
-{
+#include <stdio.h>
+
+int main() {
     int a, b;
     char op;
-    int result;
-    printf("Enter First Number:");
-    scanf("%d", &a);
-    printf("Enter Second Number:");
-    scanf("%d", &b);
-    printf("Enter Operator of the Operation you want to perform :");
+    printf("Enter Operator (+, -, *, /): ");
     scanf(" %c", &op);
-    switch (op)
-    {
-    case '+':
-        result=a+b;
-        printf("The Sum Is %d", result);
-        break;
-    case '-':
-        result=a-b;
-        printf("The Subtract Is %d", result);
-        break;
-    case '*':
-        result=a*b;
-        printf("The Multiply Is %d", result);
-        break;
-    case '/':
-        result=a/b;
-        printf("The Division Is %d", result);
-        break;
-        default:
-    printf("Please Enter a valid Operator");
-        break;
+    printf("Enter First Number: ");
+    scanf("%d", &a);
+    printf("Enter Second Number: ");
+    scanf("%d", &b);
+
+    switch (op) {
+        case '+': printf("Result: %d\n", a + b); break;
+        case '-': printf("Result: %d\n", a - b); break;
+        case '*': printf("Result: %d\n", a * b); break;
+        case '/': printf("Result: %d\n", a / b); break;
+        default: printf("Invalid operator\n"); break;
     }
     return 0;
 }
